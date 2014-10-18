fe2ea569157fc04e4dd0f2c08a6a2ad23688efa1
Giovanne <giovanne.almeida@hotmail.com>
2014-10-18 14:50:51 -0300

    Criando e deletando zonas e exibindo endereços dos marcadores

--- app/src/main/java/com/giovanne/geomaps/MainActivity.java
+++ app/src/main/java/com/giovanne/geomaps/MainActivity.java
@@ -1,5 +1,6 @@
 package com.giovanne.geomaps;
 
+import android.app.Dialog;
 import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
@@ -14,9 +16,14 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.giovanne.geomaps.adapters.MarkerCustomAdapter;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
@@ -29,13 +36,14 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends FragmentActivity {
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private ImageButton btHideList;
     private ListView lvList;
-    private Map<String,String> markersList;
-    private ArrayList<String> list;
-    private ArrayAdapter<String> adapter;
+    private Map<Marker,Circle> zoneList;
+    private ArrayList<Marker> markerList;
+    private MarkerCustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,29 +51,54 @@ public class MainActivity extends FragmentActivity {
 
         setContentView(R.layout.activity_main);
 
-        this.btHideList = (ImageButton) findViewById(R.id.btHideList);
-        btHideList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(lvList.getVisibility() == View.VISIBLE)
-                    lvList.setVisibility(View.GONE);
-                else
-                    lvList.setVisibility(View.VISIBLE);
-            }
-        });
-        this.lvList = (ListView) findViewById(R.id.lvMarkersList);
+        // Getting Google Play availability status
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
-        markersList = new HashMap<String, String>();
+        // Showing status
+        if(status!= ConnectionResult.SUCCESS){ // Google Play Services are not available
 
-        list = new ArrayList<String>();
-        list.addAll(markersList.values());
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
 
-        this.adapter = new ArrayAdapter<String>(this,R.layout.list_markers_item, R.id.text1, list);
+        }else { // Google Play Services are available
 
-        lvList.setAdapter(adapter);
-        lvList.setEmptyView(findViewById(R.id.tvEmpty));
+            this.btHideList = (ImageButton) findViewById(R.id.btHideList);
+            btHideList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (lvList.getVisibility() == View.VISIBLE)
+                        lvList.setVisibility(View.GONE);
+                    else
+                        lvList.setVisibility(View.VISIBLE);
+                }
+            });
+            this.lvList = (ListView) findViewById(R.id.lvMarkersList);
 
-        setUpMapIfNeeded();
+            zoneList = new HashMap<Marker, Circle>();
+
+            markerList = new ArrayList<Marker>();
+            markerList.addAll(zoneList.keySet());
+
+            this.adapter = new MarkerCustomAdapter(markerList, this);
+
+            lvList.setAdapter(adapter);
+            lvList.setEmptyView(findViewById(R.id.tvEmpty));
+
+            lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(markerList.get(position).getPosition())      // Sets the center of the map to Mountain View
+                            .zoom(17)                   // Sets the zoom
+//                        .bearing(90)                // Sets the orientation of the camera to east
+//                        .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                            .build();                   // Creates a CameraPosition from the builder
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                }
+            });
+            setUpMapIfNeeded();
+        }
     }
 
     @Override
@@ -98,7 +131,7 @@ public class MainActivity extends FragmentActivity {
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                removeMarker(marker);
+                removeZone(marker);
                 return true;
             }
         });
@@ -112,16 +145,16 @@ public class MainActivity extends FragmentActivity {
     }
 
     private boolean removeMarker(Marker marker) {
-        try{
-            marker.remove();
-            markersList.remove(marker.getId());
-            list.clear();
-            list.addAll(markersList.values());
-            adapter.notifyDataSetChanged();
-        } catch (Exception e){
-            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG);
-            return false;
-        }
+//        try{
+//            marker.remove();
+//            markersList.remove(marker.getId());
+//            list.clear();
+//            list.addAll(markersList.values());
+//            adapter.notifyDataSetChanged();
+//        } catch (Exception e){
+//            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG);
+//            return false;
+//        }
         return true;
     }
 
@@ -132,19 +165,19 @@ public class MainActivity extends FragmentActivity {
 //                .title("Marker")
 //                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
 
-        Marker m = mMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title("Marker"));
-        markersList.put(m.getId(),"Marker "+m.getId()+" - "+String.valueOf(latLng.latitude)+", "+String.valueOf(latLng.longitude));
-        list.clear();
-        list.addAll(markersList.values());
-        adapter.notifyDataSetChanged();
+//        Marker m = mMap.addMarker(new MarkerOptions()
+//                .position(latLng)
+//                .title("Marker"));
+//        markersList.put(m.getId(),"Marker "+m.getId()+" - "+String.valueOf(latLng.latitude)+", "+String.valueOf(latLng.longitude));
+//        list.clear();
+//        list.addAll(markersList.values());
+//        adapter.notifyDataSetChanged();
     }
 
     private void addZone(LatLng latLng){
         Circle c = mMap.addCircle(new CircleOptions()
                 .center(latLng)
-                .radius(10000)
+                .radius(50)
                 .strokeColor(Color.argb(144,255,0,0))
                 .strokeWidth(5f)
                 .fillColor(Color.argb(100,0,0,255)));
@@ -153,18 +186,19 @@ public class MainActivity extends FragmentActivity {
                 .position(latLng)
                 .title("Marker"));
 
-        markersList.put(m.getId(),c.getId());
-        list.clear();
-        list.addAll(markersList.values());
+        zoneList.put(m,c);
+        markerList.clear();
+        markerList.addAll(zoneList.keySet());
         adapter.notifyDataSetChanged();
     }
 
     private boolean removeZone(Marker marker) {
         try{
-            marker.remove();
-            markersList.remove(marker.getId());
-            list.clear();
-            list.addAll(markersList.values());
+            marker.remove(); //Remove marcador
+            zoneList.get(marker).remove(); //Remove círculo
+            zoneList.remove(marker);
+            markerList.clear();
+            markerList.addAll(zoneList.keySet());
             adapter.notifyDataSetChanged();
         } catch (Exception e){
             Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG);
---
+++ app/src/main/java/com/giovanne/geomaps/adapters/MarkerCustomAdapter.java
@@ -0,0 +1,102 @@
+package com.giovanne.geomaps.adapters;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.giovanne.geomaps.MainActivity;
+import com.giovanne.geomaps.R;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by Giovanne on 18/10/2014.
+ */
+public class MarkerCustomAdapter extends BaseAdapter {
+
+    private ArrayList<Marker> list;
+    private Context context;
+    private final LayoutInflater inflater;
+
+    public MarkerCustomAdapter(ArrayList<Marker> list, Context context) {
+        this.list = list;
+        this.context = context;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    @Override
+    public int getCount() {
+        return list != null ? list.size() : 0;
+    }
+
+    @Override
+    public Marker getItem(int position) {
+        return list != null ? list.get(position) : null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.list_markers_item, null);
+            viewHolder = new ViewHolder();
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Marker marker = list.get(position);
+
+        viewHolder.tvTitle.setText(getCompleteAddressString(marker.getPosition()));
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        protected TextView tvTitle;
+    }
+
+    private String getCompleteAddressString(LatLng latLng) {
+        String strAdd = "";
+        double LATITUDE = latLng.latitude;
+        double LONGITUDE = latLng.longitude;
+
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.w("My Current loction address", "" + strReturnedAddress.toString());
+            } else {
+                Log.w("My Current loction address", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.w("My Current loction address", "Canont get Address!");
+        }
+        return strAdd;
+    }
+}
